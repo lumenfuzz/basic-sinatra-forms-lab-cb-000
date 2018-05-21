3e704eb9b09859df16dc5eb1a9faa0357f3fb7f4
Patrick J Louis <lumenfuzz@gmail.com>
2018-05-21 20:52:29 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,12 +3,12 @@ require 'sinatra/base'
 class App < Sinatra::Base
 
   get '/newteam' do
-
     erb :newteam
   end
 
   post '/team' do
-
+    @name = params["name"]
+    @coach = params["coach"]
     erb :team
   end
 
